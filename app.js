66fbb3af230bd7b45374eced553f6c64708c72e5
florence <loracodes3@gmail.com>
2024-10-17 08:39:31 +0300

    commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const expenseForm = document.getElementById('add-expense-form');
 const expenseList = document.getElementById('expenses');
+
+function fetchExpenses(){
+    fetch (apiUrl)
+    .then(response => response.json)
+    .then (expenses => displayExpenses(expenses));
+}
